64763ae6dede72c0f3122e2fe3edecf38d3d5be7
Christina R <xsnachannell@gmail.com>
2022-08-01 22:24:18 -0700

    outlines schemas for User, Project and Skill models and then requires them into index.js and exports them out in index.js

---
+++ server/models/Project.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const { Skill } = require('./Skill');
+const { User } = require('./User');
+
+const projectSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String
+    },
+    skills:[Skill.Schema],
+    teammates:[User.Schema]
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
--- server/models/User.js
+++ server/models/User.js
@@ -3,4 +3,47 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bcrypt = require('bcrypt');
-const
+const { Project } = require('./Project');
+const { Skill } = require('./Skill');
+
+const userSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    skills:[Skill.schema],
+    projects:[Project.schema]
+});
+
+userSchema.pre('save', async function(next){
+    if(this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(pass) {
+    return await bcrypt.compare(pass, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
