2c2738202e2d7d11db77178e80d6feee520e298f
Christina R <xsnachannell@gmail.com>
2022-08-02 22:48:16 -0700

    adds connection.js file and updates server.js with basic code to connect to the apollo server and to create an instance of express to serve out the static front end build

--- server/server.js
+++ server/server.js
@@ -1,2 +1,25 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+// const { authMiddleware } = require('./utils/auth') still need to create an authentication js file
+
+//const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//creates new apollo server instance using the defined typeDefs and resolvers and authentication middleware, so that client can make queries to database. will unremark when typeDefs resolvers and middleware have been coded
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware,
+// });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
