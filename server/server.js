669aa51060a285271a16d1239f6e7d784068ba40
Christina R <xsnachannell@gmail.com>
2022-08-02 22:58:18 -0700

    updates server.js to complete code to start the apollo server and to start up the api server

--- server/server.js
+++ server/server.js
@@ -23,3 +23,21 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({app});
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}, woohoo!`);
+            console.log(`Navigate to graphql interface here http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+};
+
+startApolloServer(typeDefs, resolvers);
