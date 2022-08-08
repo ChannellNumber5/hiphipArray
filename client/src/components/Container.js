964a5091712cd673ceb4719c9f71361384080250
Pillifino <dinodizon073100@gmail.com>
2022-08-08 10:38:19 -0700

    added appollo provider along with login form logic. link from react router currently allowing user to bypass correct login authentication

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
 // Menu
 import NavTabs from "./NavTabs";
 // Pages
@@ -14,30 +23,51 @@ import Requests from "./pages/requests";
 import ChooseSkills from "./pages/ChooseSkills";
 import CreateProfile from "./pages/CreateProfile";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 export default function Container() {
   return (
-    <Router>
-      <div>
-        <NavTabs></NavTabs>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/myprofile" element={<Profile />} />
-          <Route
-            path="/Project" // for user's projects
-            element={<Project />}
-          />
-          <Route
-            path="/Projects" // for other's projects
-            element={<Projects />}
-          />
-          <Route path="Arrayers" element={<Arrayers />} />
-          <Route path="Requests" element={<Requests />} />
-          <Route path="chooseSkills" element={<ChooseSkills />} />
-          <Route path="CreateProfile" element={<CreateProfile />} />
-        </Routes>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <NavTabs></NavTabs>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/myprofile" element={<Profile />} />
+            <Route
+              path="/Project" // for user's projects
+              element={<Project />}
+            />
+            <Route
+              path="/Projects" // for other's projects
+              element={<Projects />}
+            />
+            <Route path="Arrayers" element={<Arrayers />} />
+            <Route path="Requests" element={<Requests />} />
+            <Route path="chooseSkills" element={<ChooseSkills />} />
+            <Route path="CreateProfile" element={<CreateProfile />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,52 +1,65 @@
 import React, { useState } from "react";
+import { useMutation } from '@apollo/client';
 import { Link } from "react-router-dom";
+import { LOGIN } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
-export default function Login() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+function Login(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of username or password
-    if (inputType === "password") {
-      setPassword(inputValue);
-    } else if (inputType === "userName") {
-      setUserName(inputValue);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
     }
   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
   return (
     <div>
       <h1>Login</h1>
-      <p>
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="email">Email address:</label>
         <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
+          placeholder="email"
+          name="email"
+          type="email"
+          id="email"
+          onChange={handleChange}
         />
+        <label htmlFor="pwd">Password:</label>
         <input
-          value={password}
+          placeholder="******"
           name="password"
-          onChange={handleInputChange}
           type="password"
-          placeholder="Password"
+          id="pwd"
+          onChange={handleChange}
         />
-
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
         <Link to="/myprofile">
-          <button>Login</button>
+          <button type="submit">Login</button>
         </Link>
-      </p>
+      </form>
     </div>
   );
 }
+
+export default Login;
